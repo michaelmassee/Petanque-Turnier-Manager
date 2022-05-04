898a339852bf4fb6a72fce70ec5b14ec2582fab1
Michael Massee <michael@massee.de>
2022-05-05 00:01:56 +0200

    reformat

--- src/main/java/de/petanqueturniermanager/supermelee/konfiguration/SuperMeleePropertiesSpalte.java
+++ src/main/java/de/petanqueturniermanager/supermelee/konfiguration/SuperMeleePropertiesSpalte.java
@@ -32,61 +32,83 @@ public class SuperMeleePropertiesSpalte extends BasePropertiesSpalte implements 
 	private static final String KONFIG_PROP_STREICH_SPIELTAG_COLOR_BACK_GERADE = "Streich-Spieltag Hintergrund Gerade";
 	private static final String KONFIG_PROP_STREICH_SPIELTAG_COLOR_BACK_UNGERADE = "Streich-Spieltag Hintergrund Ungerade";
 
-	private static final String KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_PLUS = "Nicht gespielte Runde Punkte +"; // 0
-	private static final String KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_MINUS = "Nicht gespielte Runde Punkte -"; // 13
+	private static final String KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_PLUS = "Nicht gespielte Runde, + Punkte"; // 0
+	private static final String KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_MINUS = "Nicht gespielte Runde, - Punkte"; // 13
 
 	private static final String KONFIG_PROP_SPIELRUNDE_SPIELBAHN = "Spielrunde Spielbahn";
-	private static final String KONFIG_PROP_ANZ_GESPIELTE_SPIELTAGE = "Anzahl gespielte Spieltage"; // anzahl spieltage die bei der neu auslosung eingelesen wird (hat zusammen
+	private static final String KONFIG_PROP_ANZ_GESPIELTE_SPIELTAGE = "Anzahl gespielte Spieltage"; // anzahl spieltage
+																									// die bei der neu
+																									// auslosung
+																									// eingelesen wird
+																									// (hat zusammen
 
-	private static final String KONFIG_PROP_SPIELRUNDE_1_HEADER = "Spielrunde, Spieltag in 1e Kopfzeile"; // spieltag in header ?
+	private static final String KONFIG_PROP_SPIELRUNDE_1_HEADER = "Spielrunde, Spieltag in 1. Kopfzeile"; // spieltag in
+																											// header ?
 
-	public static final String KONFIG_PROP_SUPERMELEE_MODE = "Supermêlée Modus"; // Default Triplette / optional Doublette
+	public static final String KONFIG_PROP_SUPERMELEE_MODE = "Supermêlée Modus"; // Default Triplette / optional
+																					// Doublette
 	private static final String KONFIG_PROP_SPIELRUNDE_PLAN = "Spielrunde Plan"; // Default false
 
 	private static final String KONFIG_PROP_SPIELTAG_KOPFZEILE = "Kopfzeile Spieltag"; // plus spieltagNr
 
 	static {
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_NAME_SPIELRUNDE_NEU_AUSLOSEN).setDefaultVal(0)
-				.setDescription("Neu auslosen ab Spielrunde").inSideBar());
+		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_NAME_SPIELRUNDE_NEU_AUSLOSEN)
+				.setDefaultVal(0).setDescription("Neu auslosen ab Spielrunde").inSideBar());
 
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_SPIELRUNDE_COLOR_BACK_GERADE).setDefaultVal(DEFAULT_GERADE_BACK_COLOR)
+		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_SPIELRUNDE_COLOR_BACK_GERADE)
+				.setDefaultVal(DEFAULT_GERADE_BACK_COLOR)
 				.setDescription("Spielrunde Hintergrundfarbe für gerade Zeilen").inSideBar());
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_SPIELRUNDE_COLOR_BACK_UNGERADE).setDefaultVal(DEFAULT_UNGERADE__BACK_COLOR)
+		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_SPIELRUNDE_COLOR_BACK_UNGERADE)
+				.setDefaultVal(DEFAULT_UNGERADE__BACK_COLOR)
 				.setDescription("Spielrunde Hintergrundfarbe für ungerade Zeilen").inSideBar());
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_SPIELRUNDE_COLOR_BACK_HEADER).setDefaultVal(DEFAULT_HEADER__BACK_COLOR)
-				.setDescription("Spielrunde Header-Hintergrundfarbe").inSideBar());
+		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_SPIELRUNDE_COLOR_BACK_HEADER)
+				.setDefaultVal(DEFAULT_HEADER__BACK_COLOR).setDescription("Spielrunde Header-Hintergrundfarbe")
+				.inSideBar());
 
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_STREICH_SPIELTAG_COLOR_BACK_GERADE).setDefaultVal(14540253)
+		KONFIG_PROPERTIES.add(ConfigProperty
+				.from(ConfigPropertyType.COLOR, KONFIG_PROP_STREICH_SPIELTAG_COLOR_BACK_GERADE).setDefaultVal(14540253)
 				.setDescription("Rangliste Hintergrundfarbe für Streich-Spieltag gerade Zeilen").inSideBar());
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_STREICH_SPIELTAG_COLOR_BACK_UNGERADE).setDefaultVal(Integer.valueOf("ccc8c1", 16))
-				.setDescription("Rangliste Hintergrundfarbe für Streich-Spieltag ungerade Zeilen").inSideBar());
+		KONFIG_PROPERTIES
+				.add(ConfigProperty.from(ConfigPropertyType.COLOR, KONFIG_PROP_STREICH_SPIELTAG_COLOR_BACK_UNGERADE)
+						.setDefaultVal(Integer.valueOf("ccc8c1", 16))
+						.setDescription("Rangliste Hintergrundfarbe für Streich-Spieltag ungerade Zeilen").inSideBar());
 
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_PLUS).setDefaultVal(0)
-				.setDescription("Pluspunkte nicht gespielte Runde").inSideBar());
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_MINUS).setDefaultVal(13)
-				.setDescription("Minuspunkte nicht gespielte Runde").inSideBar());
+		KONFIG_PROPERTIES
+				.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_PLUS)
+						.setDefaultVal(0).setDescription("Pluspunkte nicht gespielte Runde").inSideBar());
+		KONFIG_PROPERTIES
+				.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_RANGLISTE_NICHT_GESPIELTE_RND_MINUS)
+						.setDefaultVal(13).setDescription("Minuspunkte nicht gespielte Runde").inSideBar());
 
-		KONFIG_PROPERTIES.add(((AuswahlConfigProperty) AuswahlConfigProperty.from(KONFIG_PROP_SPIELRUNDE_SPIELBAHN).setDefaultVal("X")
-				.setDescription("Spalte Spielbahn in Spielrunde.\r\nX=Keine Spalte\r\nL=Leere Spalte (händisch ausfüllen)\r\nN=Durchnummerieren\r\nR=Random"))
-						.addAuswahl("X", "Keine Spalte").addAuswahl("L", "Leere Spalte").addAuswahl("N", "Durchnummerieren (1-n)").addAuswahl("R", "Zufällig vergeben")
-						.inSideBar());
+		KONFIG_PROPERTIES.add(((AuswahlConfigProperty) AuswahlConfigProperty.from(KONFIG_PROP_SPIELRUNDE_SPIELBAHN)
+				.setDefaultVal("X").setDescription(
+						"Spalte Spielbahn in Spielrunde.\r\nX=Keine Spalte\r\nL=Leere Spalte (händisch ausfüllen)\r\nN=Durchnummerieren\r\nR=Random"))
+				.addAuswahl("X", "Keine Spalte").addAuswahl("L", "Leere Spalte")
+				.addAuswahl("N", "Durchnummerieren (1-n)").addAuswahl("R", "Zufällig vergeben").inSideBar());
 
-		KONFIG_PROPERTIES.add(((AuswahlConfigProperty) AuswahlConfigProperty.from(KONFIG_PROP_SUPERMELEE_MODE).setDefaultVal("T")
-				.setDescription("Spielrunden Modus\r\nT=Triplette\r\nD=Doublette")).addAuswahl("T", "Triplette").addAuswahl("D", "Doublette").inSideBar());
+		KONFIG_PROPERTIES.add(((AuswahlConfigProperty) AuswahlConfigProperty.from(KONFIG_PROP_SUPERMELEE_MODE)
+				.setDefaultVal("T").setDescription("Spielrunden Modus\r\nT=Triplette\r\nD=Doublette"))
+				.addAuswahl("T", "Triplette").addAuswahl("D", "Doublette").inSideBar());
 
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_ANZ_GESPIELTE_SPIELTAGE).setDefaultVal(99)
-				.setDescription("Die Anzahl vergangene Spieltage die bei der Auslosung von neuen Spielrunden eingelesen werden. (Hat zusammen gespielt mit)").inSideBar());
+		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.INTEGER, KONFIG_PROP_ANZ_GESPIELTE_SPIELTAGE)
+				.setDefaultVal(99)
+				.setDescription(
+						"Die Anzahl vergangene Spieltage die bei der Auslosung von neuen Spielrunden eingelesen werden. (Hat zusammen gespielt mit)")
+				.inSideBar());
 
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.BOOLEAN, KONFIG_PROP_SPIELRUNDE_1_HEADER).setDefaultVal(false).inSideBar()
-				.setDescription("Spielrunde, 1. Headerzeile mit Spieltag Info\r\nN/J (default=N)"));
+		KONFIG_PROPERTIES.add(
+				ConfigProperty.from(ConfigPropertyType.BOOLEAN, KONFIG_PROP_SPIELRUNDE_1_HEADER).setDefaultVal(false)
+						.inSideBar().setDescription("Spielrunde, 1. Headerzeile mit Spieltag Info\r\nN/J (default=N)"));
 
-		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.BOOLEAN, KONFIG_PROP_SPIELRUNDE_PLAN).setDefaultVal(false).inSideBar()
-				.setDescription("Erstelle ein Spielrunde Plan zur jeder Spielrunde\r\nN/J (default=N)"));
+		KONFIG_PROPERTIES.add(ConfigProperty.from(ConfigPropertyType.BOOLEAN, KONFIG_PROP_SPIELRUNDE_PLAN)
+				.setDefaultVal(false).inSideBar()
+				.setDescription("Erstelle ein Spielrunde Plan zu jeder Spielrunde\r\nN/J (default=N)"));
 
 		// Spieltag Header
 		for (int spieltagcntr = 1; spieltagcntr <= SuperMeleeKonfigurationSheet.MAX_SPIELTAG; spieltagcntr++) {
-			KONFIG_PROPERTIES.add(HeaderFooterConfigProperty.from(PROP_SPIELTAG_KOPFZEILE(spieltagcntr)).setDefaultVal(spieltagcntr + ". Spieltag")
-					.setDescription("Kopfzeile für Spieltag " + spieltagcntr).inSideBar());
+			KONFIG_PROPERTIES.add(HeaderFooterConfigProperty.from(PROP_SPIELTAG_KOPFZEILE(spieltagcntr))
+					.setDefaultVal(spieltagcntr + ". Spieltag").setDescription("Kopfzeile für Spieltag " + spieltagcntr)
+					.inSideBar());
 		}
 	}
 
