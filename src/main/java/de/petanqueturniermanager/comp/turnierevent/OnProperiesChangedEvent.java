71fff6a01b22896c251ba7cd56ce999092aa5bde
Michael Massee <michael@massee.de>
2022-06-10 15:58:09 +0200

    prop changed event mit liste von geaenderte properties

--- src/main/java/de/petanqueturniermanager/comp/turnierevent/OnProperiesChangedEvent.java
+++ src/main/java/de/petanqueturniermanager/comp/turnierevent/OnProperiesChangedEvent.java
@@ -5,6 +5,10 @@ package de.petanqueturniermanager.comp.turnierevent;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
 import com.sun.star.sheet.XSpreadsheetDocument;
 
 /**
@@ -14,9 +18,15 @@ import com.sun.star.sheet.XSpreadsheetDocument;
 public class OnProperiesChangedEvent implements ITurnierEvent {
 
 	private final XSpreadsheetDocument xSpreadsheetDocument;
+	private HashMap<String, List> changed = new HashMap<>();
 
 	public OnProperiesChangedEvent(XSpreadsheetDocument xSpreadsheetDocument) {
 		this.xSpreadsheetDocument = checkNotNull(xSpreadsheetDocument);
+	}
+
+	public OnProperiesChangedEvent addChanged(String propName, String oldVal, String newVal) {
+		changed.put(propName, Arrays.asList(oldVal, newVal));
+		return this;
 	}
 
 	@Override
