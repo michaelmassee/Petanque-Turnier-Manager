73659c7c7b3fb66864cd9c1a9f94e7f84dcdc530
Michael Massee <michael@massee.de>
2022-06-26 22:55:34 +0200

    style fuer green und red

---
+++ src/main/java/de/petanqueturniermanager/helper/cellstyle/RanglisteHintergrundFarbeGeradeStyleGreen.java
@@ -0,0 +1,15 @@
+/**
+* Erstellung : 21.05.2018 / Michael Massee
+**/
+
+package de.petanqueturniermanager.helper.cellstyle;
+
+import de.petanqueturniermanager.helper.ColorHelper;
+
+public class RanglisteHintergrundFarbeGeradeStyleGreen extends AbstractVordergrundHintergrundFarbeStyle {
+
+	public RanglisteHintergrundFarbeGeradeStyleGreen(Integer geradeColor) {
+		super(CellStyleDefName.RanglisteHintergrundFarbeGeradeGreen, geradeColor, ColorHelper.CHAR_COLOR_GREEN);
+	}
+
+}
