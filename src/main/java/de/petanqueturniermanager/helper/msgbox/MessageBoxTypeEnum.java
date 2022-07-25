b5d04ba4bfd8d434bcf3278cc677617e4a47a9e5
Michael Massee <michael@massee.de>
2022-07-25 23:58:11 +0200

    msbox weitere varianten yes no cancel

--- src/main/java/de/petanqueturniermanager/helper/msgbox/MessageBoxTypeEnum.java
+++ src/main/java/de/petanqueturniermanager/helper/msgbox/MessageBoxTypeEnum.java
@@ -1,9 +1,9 @@
 /**
-* Erstellung : 26.05.2018 / Michael Massee
-**/
+ * Erstellung : 26.05.2018 / Michael Massee
+ **/
 
 package de.petanqueturniermanager.helper.msgbox;
 
 public enum MessageBoxTypeEnum {
-	QUESTION_YES_NO, QUESTION_OK_CANCEL, WARN_OK, WARN_YES_NO, WARN_OK_CANCEL, ERROR_OK, INFO_OK
+	QUESTION_YES_NO, QUESTION_OK_CANCEL, WARN_OK, WARN_YES_NO, WARN_YES_NO_CANCEL, WARN_OK_CANCEL, ERROR_OK, INFO_OK
 }
