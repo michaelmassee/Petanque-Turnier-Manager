92b7b408132c7a39debc3a3d3a838ac481b7f902
Michael Massee <michael@massee.de>
2022-08-03 23:26:26 +0200

    validate json nach base verschoben

--- src/test/java/de/petanqueturniermanager/BaseCalcUITest.java
+++ src/test/java/de/petanqueturniermanager/BaseCalcUITest.java
@@ -1,12 +1,27 @@
 package de.petanqueturniermanager;
 
-import java.io.IOException;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonIOException;
 import com.sun.star.frame.XComponentLoader;
 import com.sun.star.sheet.XSpreadsheetDocument;
 
@@ -15,12 +30,17 @@ import de.petanqueturniermanager.comp.OfficeStarter;
 import de.petanqueturniermanager.comp.WorkingSpreadsheet;
 import de.petanqueturniermanager.helper.msgbox.ProcessBox;
 import de.petanqueturniermanager.helper.sheet.SheetHelper;
+import de.petanqueturniermanager.helper.sheet.rangedata.CellData;
+import de.petanqueturniermanager.helper.sheet.rangedata.RangeData;
+import de.petanqueturniermanager.helper.sheet.rangedata.RowData;
 
 /**
  * Erstellung 13.07.2022 / Michael Massee
  */
 
 public abstract class BaseCalcUITest {
+
+	private static final Logger logger = LogManager.getLogger(BaseCalcUITest.class);
 
 	protected static OfficeStarter starter = OfficeStarter.from();
 	protected static XComponentLoader loader;
@@ -65,6 +85,39 @@ public abstract class BaseCalcUITest {
 	protected void waitEnter() throws IOException {
 		System.out.println("Press Enter .....");
 		System.in.read();
+	}
+
+	protected void validateWithJson(RangeData rangeData, InputStream jsonFile) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		RangeData refRangeData = gson.fromJson(new BufferedReader(new InputStreamReader(jsonFile)), RangeData.class);
+
+		assertThat(rangeData).hasSameSizeAs(refRangeData);
+
+		int idx = 0;
+		// jede zeile vergleichen, wegen fehlermeldung 
+		for (RowData data : refRangeData) {
+			List<String> expected = refRangeData.get(idx).stream().map(c -> c.getStringVal())
+					.collect(Collectors.toList());
+			logger.info("Validate Zeile :" + expected);
+			assertThat(data).extracting(CellData::getStringVal).containsExactlyElementsOf(expected);
+			idx++;
+		}
+	}
+
+	protected void writeToJson(String fileName, RangeData rangeData) {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+		try {
+			File jsoFile = new File("/home/michael/tmp/", fileName);
+			try (BufferedWriter fileStream = new BufferedWriter(new FileWriter(jsoFile))) {
+				fileStream.write(gson.toJson(rangeData));
+			}
+		} catch (JsonIOException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
