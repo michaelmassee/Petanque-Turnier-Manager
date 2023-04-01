232e92d99792bb6f936ca9feafd897c6ee5c3737
Michael Massee <michael@massee.de>
2023-04-01 14:32:40 +0200

    leere constructur ohne sheet in *cellvalue klassen entfernt

--- src/main/java/de/petanqueturniermanager/helper/cellvalue/IntegerCellValue.java
+++ src/main/java/de/petanqueturniermanager/helper/cellvalue/IntegerCellValue.java
@@ -1,15 +1,21 @@
 /**
-* Erstellung : 05.04.2018 / Michael Massee
-**/
+ * Erstellung : 05.04.2018 / Michael Massee
+ **/
 
 package de.petanqueturniermanager.helper.cellvalue;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import com.sun.star.sheet.XSpreadsheet;
+
 public class IntegerCellValue extends NumberCellValue {
 
+	public IntegerCellValue(XSpreadsheet sheet) {
+		super(sheet);
+	}
+
 	public static final IntegerCellValue from(StringCellValue cellVal) {
-		IntegerCellValue numberCellValue = new IntegerCellValue();
+		IntegerCellValue numberCellValue = new IntegerCellValue(cellVal.getSheet());
 		numberCellValue.copyCommonAttr(cellVal).setValue(NumberUtils.toDouble(cellVal.getValue(), -1));
 		return numberCellValue;
 	}
